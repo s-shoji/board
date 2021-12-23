2a1f6b26192186ddec4dfeddad58583c547320f7
shoji <s.shoji39@gmail.com>
2021-12-23 21:31:15 +0900

    投稿機能の作成

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight flex items-center justify-between mt-4 p-6 space-x-5">
+            {{ __('Post') }}
+            <!-- <a href="{{ route('posts.create') }}">{{ __('TweetCreate') }}</a> -->
+        </h2>  
+   
+        <!-- <h2>今月の累計投稿数は{{$sumt ?? ''}}です。</h2>    -->
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                @foreach($posts as $post)
+                <div class="text-center text-2xl" >
+                  
+                  title:
+                  {{$post->title}} <br>
+                  content:
+                  {{$post->content}} <br>
+                  created_at:
+                  {{$post->created_at}} <br>
+                  user_name:
+                  {{$post->user->name}}<br><br>
+                  @endforeach
+                </div>
+             
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('posts/index','PostController@index')->name('posts.index');
+//表示用
+Route::get('posts/create','PostController@create')->name('posts.create');
+//投稿を押した時
+Route::post('posts/create','PostController@store');
+
